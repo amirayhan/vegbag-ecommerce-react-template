bcfb51ad56d489a8841e77b349f7409c3f33cb28
amirayhan <jaber.ac1983@gmail.com>
2023-10-02 18:27:29 +0600

    header, home and layout route setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Layout/Main";
+import Home from "./components/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+        ],
     },
 ]);
 
