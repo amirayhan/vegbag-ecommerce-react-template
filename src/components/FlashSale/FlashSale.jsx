7bb1489f58e214f8ba83ddf8fc80daf95d5a8939
amirayhan <jaber.ac1983@gmail.com>
2023-10-13 17:48:03 +0600

    flash sale section added

--- src/components/FlashSale/FlashSale.jsx
+++ src/components/FlashSale/FlashSale.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./FlashSale.css";
 import ProductItem from "../ProductItem/ProductItem";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 const FlashSale = () => {
     const [products, setPorducts] = useState([]);
@@ -9,15 +12,53 @@ const FlashSale = () => {
             .then((res) => res.json())
             .then((data) => setPorducts(data));
     }, []);
+
+    const settings = {
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        arrows: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
     return (
         <>
             <div>
                 <div className="container">
                     <h2 className="pb-2">Flash Sale</h2>
                     <div className="row">
-                        {products.map((product) => (
-                            <ProductItem key={product.id} product={product}></ProductItem>
-                        ))}
+                        <Slider {...settings}>
+                            {products.map((product) => (
+                                <ProductItem key={product.id} product={product}></ProductItem>
+                            ))}
+                        </Slider>
                     </div>
                 </div>
             </div>
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -6,23 +6,21 @@ const ProductItem = ({ product }) => {
     const { image, title, privPrice, newPrice, ratings, totalRatings } = product;
     console.log(product);
     return (
-        <div className="col-lg-3">
-            <div className="product_item">
-                <img src={image} alt={title} />
-                <h3>{title}</h3>
-                <div className="flex star">
-                    {[...Array(ratings)].map((_, index) => (
-                        <HiStar key={index}></HiStar>
-                    ))}
-                    {[...Array(5 - ratings)].map((_, index) => (
-                        <HiOutlineStar key={index}></HiOutlineStar>
-                    ))}
-                </div>
-                <p>
-                    <span className="text-decoration-line-through">${privPrice}</span>
-                    <span className="bold">${newPrice}</span>
-                </p>
+        <div className="product_item">
+            <img src={image} alt={title} />
+            <h4>{title}</h4>
+            <div className="flex star">
+                {[...Array(ratings)].map((_, index) => (
+                    <HiStar key={index}></HiStar>
+                ))}
+                {[...Array(5 - ratings)].map((_, index) => (
+                    <HiOutlineStar key={index}></HiOutlineStar>
+                ))}
             </div>
+            <p>
+                <span className="text-decoration-line-through">${privPrice}</span>
+                <span className="bold">${newPrice}</span>
+            </p>
         </div>
     );
 };
